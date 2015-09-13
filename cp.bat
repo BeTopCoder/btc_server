26856c1e22a4b17a48228e638257f58796f3e707
stone-jin <jb1520006273@gmail.com>
2015-09-13 22:29:21 +0800

    给工程增加自动化拷贝工具，为后续CI做准备

---
+++ cp.bat
@@ -0,0 +1,7 @@
+rem 创建文件夹
+mkdir proj\btc_server\x64
+mkdir proj\btc_server\x64\Debug\
+mkdir proj\btc_server\x64\Release\
+
+copy thirdpart\dll\pgxx\* proj\btc_server\x64\Debug\
+copy thirdpart\dll\pgxx\* proj\btc_server\x64\Release\
