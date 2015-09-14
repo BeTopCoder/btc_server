1ccf925fa903e262096101270c9c77c732468ffa
stone-jin <jb1520006273@gmail.com>
2015-09-14 23:48:16 +0800

    最近把boost asio框架搭建完，还有回调的表也处理下

--- src/httpmessage_server/httpmessage_server.cpp
+++ src/httpmessage_server/httpmessage_server.cpp
@@ -27,10 +27,38 @@ namespace httpmessage_server
         boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver_.resolve(query_, ec);
         if (ec)
         {
-
+            LOG_ERR << "HTTP Server bind address, DNS resolve failed" << ec.message() << ", address:" << m_strAddress_;
+            throw std::exception("DNS resolve failed");
         }
 
+        boost::asio::ip::tcp::endpoint endpoint_ = *endpoint_iterator;
+        m_acceptor_.open(endpoint_.protocol(), ec);
+        if (ec)
+        {
+            LOG_ERR << "HTTP Server open protocol failed: " << ec.message();
+            throw std::exception("HTTP Server open protocol failed");
+        }
 
+        m_acceptor_.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true), ec);
+        if (ec)
+        {
+            LOG_ERR << "HTTP Server open protocol failed: " << ec.message();
+            throw std::exception("HTTP Server open protocol failed");
+        }
+
+        m_acceptor_.bind(endpoint_, ec);
+        if (ec)
+        {
+            LOG_ERR << "HTTP Server bind failed: " << ec.message() << ", address: " << m_strAddress_;
+            throw std::exception("HTTP bind failed");
+        }
+
+        m_acceptor_.listen(boost::asio::socket_base::max_connections, ec);
+        if (ec)
+        {
+            LOG_ERR << "HTTP Server listen failed: " << ec.message();
+            throw std::exception("HTTP Server listen failed");
+        }
     }
 
     void http_server::start()
