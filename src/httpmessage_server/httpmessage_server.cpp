6bb6bca6e67c3e0bbe087a133fa2ed97524dff1e
stone-jin <jb1520006273@gmail.com>
2015-09-14 23:29:41 +0800

    给工程加入了打日志的功能，当前打日志功能是从AVROUTER工程中剥离出来

--- src/httpmessage_server/httpmessage_server.cpp
+++ src/httpmessage_server/httpmessage_server.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
+#include "httpmessage_server.h"
+#include "logging.hpp"
 using namespace std;
+
+namespace httpmessage_server
+{
+    http_server::http_server(boost::asio::io_service &io_servce_, unsigned short usPort, std::string address)
+        : m_io_service_(io_servce_)
+        , m_acceptor_(m_io_service_)
+        , m_usPort(usPort)
+        , m_strAddress_(address)
+    {
+
+    }
+    http_server::~http_server()
+    {
+    }
+
+    void http_server::init()
+    {
+        boost::asio::ip::tcp::resolver resolver_(m_io_service_);
+        stringstream strPort_;
+        strPort_ << m_usPort;
+        boost::system::error_code ec;
+        boost::asio::ip::tcp::resolver::query query_(m_strAddress_, strPort_.str());
+        boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver_.resolve(query_, ec);
+        if (ec)
+        {
+
+        }
+
+
+    }
+
+    void http_server::start()
+    {
+    }
+
+    void http_server::stop()
+    {
+    }
+}
+
 
 int main(int argc, char **argv)
 {
+    INIT_LOGGER("example.log");
+    LOG_ERR << "123";
     return 0;
 }
---
+++ src/httpmessage_server/httpmessage_server.h
@@ -0,0 +1,33 @@
+#ifndef __HTTPMESSAGE_SERVER_H__
+#define __HTTPMESSAGE_SERVER_H__
+
+#include <boost/asio.hpp>
+
+namespace httpmessage_server
+{
+    class http_server
+        : public boost::noncopyable
+    {
+    public:
+        explicit http_server(boost::asio::io_service &io_servce_, unsigned short usPort, std::string address = "0.0.0.0");
+
+        ~http_server();
+
+    public:
+        void init();
+
+        void start();
+
+        void stop();
+
+    protected:
+    private:
+        boost::asio::io_service &m_io_service_;
+        boost::asio::ip::tcp::acceptor m_acceptor_;
+
+        unsigned short m_usPort;
+        std::string m_strAddress_;
+    };
+}
+
+#endif
