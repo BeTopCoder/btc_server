7132cee1fa82a2c79101adab55590170020b7cfc
stone-jin <jb1520006273@gmail.com>
2015-09-18 23:47:26 +0800

    明天做http的解析那块的代码

--- src/httpmessage_server/http_connection.hpp
+++ src/httpmessage_server/http_connection.hpp
@@ -21,6 +21,7 @@ namespace httpmessage_server
             : m_io_service_(io_service_)
             , m_socket_(m_io_service_)
             , m_http_server_(http_server_)
+            , m_abort(false)
             , m_http_connection_manager_(http_connection_manager_)
         {
 
@@ -29,12 +30,36 @@ namespace httpmessage_server
     public:
         void start()
         {
+            m_request_.consume(m_request_.size());
 
+            boost::system::error_code ec;
+            m_socket_.set_option(boost::asio::ip::tcp::no_delay(true), ec);
+            if (ec)
+            {
+                LOG_ERR << "http_connection:start, Set Options to nodelay, error message: " << ec.message();
+                throw std::exception("error:set Options");
+            }
+            auto self = shared_from_this();
+            boost::asio::async_read(m_socket_, m_request_, [this, self](boost::system::error_code ec, std::size_t length) {
+                if (ec || m_abort)
+                {
+                    m_http_connection_manager_.stop(shared_from_this());
+                    return;
+                }
+
+                /* ¸´ÖÆhttpÍ·»º³åÇø */
+                std::vector<char> buffer_;
+                buffer_.resize(length + 1);
+                buffer_[length] = 0;
+                m_request_.sgetn(&buffer_[0], length);
+            });
         }
 
         void stop()
         {
-
+            boost::system::error_code ec;
+            m_abort = true;
+            m_socket_.close(ec);
         }
 
         boost::asio::ip::tcp::socket& getSocket()
@@ -52,7 +77,9 @@ namespace httpmessage_server
         boost::asio::io_service &m_io_service_;
         boost::asio::ip::tcp::socket m_socket_;
         boost::asio::ip::tcp::endpoint m_endpoint_;
+        boost::asio::streambuf m_request_;
         http_server &m_http_server_;
+        bool m_abort;
 
         http_connection_manager &m_http_connection_manager_;
     };
